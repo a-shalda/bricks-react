546638b2c7db547568ffcde512c35b48fe5351c7
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2024-01-22 13:49:45 +0400

    completed animation for categories

--- components/main/categories.jsx
+++ components/main/categories.jsx
@@ -3,19 +3,35 @@
 import "@/scss/styles.scss";
 import categoriesData from "@/data/categories"
 import { useState } from "react"
+import Image from "next/image";
 
 
 const Category = ({ i }) => {
 
   const [enlarge, setEnlarge] = useState("")
+  const [contBlurred, setContBlurred] = useState("cont_blurred")
+  const [imgUnloaded, setImgUnloaded] = useState("img_unloaded")
 
+  const handleLoad = () => {
+    setContBlurred("")
+    setImgUnloaded("")
+  }
   const handleMouseEnter = () => setEnlarge("enlarge")
   const handleMouseLeave = () => setEnlarge("")
 
   return (
-    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`categories__box ${categoriesData[i].id}`}>
+    <div 
+      onMouseEnter={handleMouseEnter} 
+      onMouseLeave={handleMouseLeave} 
+      className={`categories__box ${categoriesData[i].id} ${contBlurred}`}
+    >
         <a href={`${categoriesData[i].path}`}>
-          <img className={`categories__box__img cat_img${[i]} ${enlarge}`} src={categoriesData[i].img} alt={`Category ${categoriesData[i].desc}`} loading="lazy" width="575" height="260"></img>
+          <Image 
+            onLoad={handleLoad}
+            className={`categories__box__img cat_img${[i]} ${enlarge} ${imgUnloaded}`} 
+            src={categoriesData[i].img} alt={`Category ${categoriesData[i].desc}`} 
+            loading="lazy" width="575" height="260"
+          />
           <div className="categories__box__title">
               <h2 className="categories__box__title__h2">{categoriesData[i].title}</h2>
           </div>
