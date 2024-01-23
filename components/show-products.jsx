19b893415dec0a623cd6818a2554cff8b4ec0b65
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2024-01-23 10:47:34 +0400

    refactored show-products

--- components/show-products.jsx
+++ components/show-products.jsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 import { useState } from "react"
 
 
-const ShowCategoryProducts = () => {
+const ShowProducts = () => {
 
   let productsHTML = [];
 
   products.forEach((product, index) => {
-    productsHTML.push(<ShowProducts key={product.id} product={product} index={index} />);
+    productsHTML.push(<GenerateProducts key={product.id} product={product} index={index} />);
   });
 
   return (
@@ -20,7 +20,7 @@ const ShowCategoryProducts = () => {
   )
 }
 
-const ShowProducts = ({ product, index }) => {
+const GenerateProducts = ({ product, index }) => {
 
   let priceCentsM2 = product.priceCentsM2;
   let priceCentsPc = product.priceCentsPc;
@@ -32,40 +32,55 @@ const ShowProducts = ({ product, index }) => {
 
   if (isM2 === true && supplierPriceType === 'm2' && supplierPriceType !== 'pc') {
 
-    return <TypeOne
+    const priceM = ((priceCentsM2 / 100).toFixed(2));
+    const pricePc = (Math.ceil((priceCentsM2 / piecesInSquareMeter).toFixed(4)) / 100).toFixed(2);
+
+    return <ProductCard
       product={product}
       index={index}
-      priceCentsM2={priceCentsM2}
-      piecesInSquareMeter={piecesInSquareMeter}
+      priceM={priceM}
+      pricePc={pricePc}
+      type={"one"}
     />
   }
   else if (product.supplierPriceType === 'pc') {
 
     if (isM2 === true && isLinearMeter === false) {
 
-      return <TypeTwo
+      const priceM = (Math.ceil((priceCentsPc * piecesInSquareMeter).toFixed(4)) / 100).toFixed(2);
+      const pricePc = (priceCentsPc / 100).toFixed(2).toString();
+
+      return <ProductCard
         product={product}
         index={index}
-        priceCentsPc={priceCentsPc}
-        piecesInSquareMeter={piecesInSquareMeter}
+        priceM={priceM}
+        pricePc={pricePc}
+        type={"two"}
       />
     }
     else if (isM2 === false && isLinearMeter === true) {
 
-      return <TypeThree
+      const priceM = (Math.ceil((priceCentsPc * piecesInLinearMeter).toFixed(4)) / 100).toFixed(2).toString();
+      const pricePc = (priceCentsPc / 100).toFixed(2).toString();
+
+      return <ProductCard
         product={product}
         index={index}
-        priceCentsPc={priceCentsPc}
-        piecesInLinearMeter={piecesInLinearMeter}
+        priceM={priceM}
+        pricePc={pricePc}
+        type={"three"}
       />
     }
 
     else if (isM2 === false && isLinearMeter === false) {
 
-      return <TypeFour
+      const pricePc = (priceCentsPc / 100).toFixed(2).toString();
+
+      return <ProductCard
         product={product}
         index={index}
-        priceCentsPc={priceCentsPc}
+        pricePc={pricePc}
+        type={"four"}
       />
     }
   }
@@ -74,12 +89,10 @@ const ShowProducts = ({ product, index }) => {
 let timeOut = ''
 let timeOutBack = ''
 
-const TypeOne = ({ product, index, priceCentsM2, piecesInSquareMeter }) => {
+const ProductCard = ({ product, index, priceM, pricePc, type }) => {
 
-  const priceM2 = ((priceCentsM2 / 100).toFixed(2));
-  const pricePc = (Math.ceil((priceCentsM2 / piecesInSquareMeter).toFixed(4)) / 100).toFixed(2);
-  const indexOfDotM2 = priceM2.toString().indexOf('.');
-  const indexofDotPc = pricePc.toString().indexOf('.');
+  if (!product) return null
+  const indexOfDot = (number) => number.toString().indexOf('.')
 
   const [contBlurred, setContBlurred] = useState("cont_blurred")
   const [imgUnloaded, setImgUnloaded] = useState("img_unloaded")
@@ -107,37 +120,80 @@ const TypeOne = ({ product, index, priceCentsM2, piecesInSquareMeter }) => {
     clearTimeout(timeOutBack);
   }
 
-  let priceM2HTML = (
+  let priceType = (
     <>
-      <sup>€</sup>{priceM2.slice(0, indexOfDotM2)}<span className="price-small">{priceM2.slice(indexOfDotM2)}</span> <span className="price-desc">m<sup>2</sup></span>
+      &nbsp;m<sup>2</sup>
     </>
   )
 
-  let pricePcHTML = (
+  if (type === "three") {
+    priceType = (
+      <>
+        &nbsp;lm
+      </>
+    )
+  }
+
+  let priceFinalM = <></>
+
+  (type !== "four") ? priceFinalM = (
     <>
-      <sup>€</sup>{pricePc.slice(0, indexofDotPc)}<span className="price-small">{pricePc.slice(indexofDotPc)}</span> <span className="price-desc">pc</span>
+      <sup>€</sup>
+      {priceM.slice(0, indexOfDot(priceM))}
+      <span className="price-small">
+        {priceM.slice(indexOfDot(priceM))}
+      </span>
+      <span className="price-desc">
+        {priceType}
+      </span>
+    </>
+  ) : null
+
+  let priceFinalPc = (
+    <>
+      <sup>€</sup>
+      {pricePc.slice(0, indexOfDot(pricePc))}
+      <span className="price-small">
+        {pricePc.slice(indexOfDot(pricePc))}
+      </span>
+      <span className="price-desc">&nbsp;pc</span>
     </>
   )
 
   return (
-
-    <div className="product" onPointerEnter={handlePointerEnter} onPointerLeave={handlePointerLeave}>
+    <div
+      className="product"
+      onPointerEnter={handlePointerEnter}
+      onPointerLeave={handlePointerLeave}>
 
       <div className="product__top">
         <a href={`${product.filepath}`}>
           <div className={`product__top__cont ${contBlurred}`}>
             <Image
               onLoad={handleLoad}
-              className={`product__top__cont__img product_img_${index} ${imgUnloaded} ${opacity} ${moveLeft} ${moveRight}`}
+              className={`
+                product__top__cont__img product_img_${index} 
+                ${imgUnloaded} 
+                ${opacity} 
+                ${moveLeft} 
+                ${moveRight}
+              `}
               src={`${product.image_thumbnail[0]}`}
               alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`}
-              width="350" height="229" loading="lazy"
+              width="350" height="229"
+              loading="lazy"
             />
             <Image
-              className={`product__top__cont__img product_img_second_${index} ${opacity} ${moveLeft} ${moveRight}`}
+              className={`
+                product__top__cont__img product_img_second_${index} 
+                ${opacity} 
+                ${moveLeft} 
+                ${moveRight}
+              `}
               src={`${product.image_thumbnail[1]}`}
               alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`}
-              width="350" height="229" loading="eager"
+              width="350" height="229"
+              loading="eager"
             />
           </div>
         </a>
@@ -150,10 +206,10 @@ const TypeOne = ({ product, index, priceCentsM2, piecesInSquareMeter }) => {
 
       <div className="product__middle">
         <div className="product__middle__price--m2">
-          <p className="product__middle__price--m2__p">{priceM2HTML}</p>
+          <p className="product__middle__price--m2__p">{(type !== "four") ? priceFinalM : priceFinalPc}</p>
         </div>
         <div className="product__middle__price--st">
-          <p className="product__middle__price--st__p">{pricePcHTML}</p>
+          <p className="product__middle__price--st__p">{(type !== "four") ? priceFinalPc : null}</p>
         </div>
       </div>
 
@@ -171,249 +227,4 @@ const TypeOne = ({ product, index, priceCentsM2, piecesInSquareMeter }) => {
   )
 }
 
-const TypeTwo = ({ product, index, priceCentsPc, piecesInSquareMeter }) => {
-
-  const priceM2 = (Math.ceil((priceCentsPc * piecesInSquareMeter).toFixed(4)) / 100).toFixed(2);
-  const pricePc = (priceCentsPc / 100).toFixed(2).toString();
-  const indexOfDotM2 = priceM2.toString().indexOf('.');
-  const indexofDotPc = pricePc.toString().indexOf('.');
-
-  const [contBlurred, setContBlurred] = useState("cont_blurred")
-  const [imgUnloaded, setImgUnloaded] = useState("img_unloaded")
-
-  const handleLoad = () => {
-    setContBlurred("")
-    setImgUnloaded("")
-  }
-
-  const [opacity, setOpacity] = useState("")
-  const [moveLeft, setMoveLeft] = useState("")
-  const [moveRight, setMoveRight] = useState("")
-
-  const handlePointerEnter = () => {
-    setOpacity("opacity")
-    timeOut = setTimeout(() => setMoveLeft("moveLeft"), 1000)
-    timeOutBack = setTimeout(() => setMoveRight("moveRight"), 5000);
-  }
-
-  const handlePointerLeave = () => {
-    setOpacity("")
-    setMoveLeft("")
-    setMoveRight("")
-    clearTimeout(timeOut);
-    clearTimeout(timeOutBack);
-  }
-
-  let priceM2HTML = (
-    <>
-      <sup>€</sup>{priceM2.slice(0, indexOfDotM2)}<span className="price-small">{priceM2.slice(indexOfDotM2)}</span> <span className="price-desc">m<sup>2</sup></span>
-    </>
-
-  )
-  let pricePcHTML = (
-    <>
-      <sup>€</sup>{pricePc.slice(0, indexofDotPc)}<span className="price-small">{pricePc.slice(indexofDotPc)}</span> <span className="price-desc">pc</span>
-    </>
-  )
-
-  return (
-    <div className="product" onPointerEnter={handlePointerEnter} onPointerLeave={handlePointerLeave}>
-      <div className="product__top">
-
-        <a href={`${product.filepath}`}>
-          <div className={`product__top__cont ${contBlurred}`}>
-            <Image onLoad={handleLoad} className={`product__top__cont__img product_img_${index} ${imgUnloaded} ${opacity} ${moveLeft} ${moveRight}`} src={`${product.image_thumbnail[0]}`} alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`} width="350" height="229" loading="lazy" />
-            <Image className={`product__top__cont__img product_img_second_${index} ${opacity} ${moveLeft} ${moveRight}`} src={`${product.image_thumbnail[1]}`} alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`} width="350" height="229" loading="eager"/>
-          </div>
-        </a>
-
-        <div className="product__top__cont__stock">
-          <Image src="/images/icons/check.svg" alt="checkmark" className="stock" width="16" height="16" />
-          <p className="product__top__cont__stock__desc">{product.availability}</p>
-        </div>
-      </div>
-
-      <div className="product__middle">
-        <div className="product__middle__price--m2">
-          <p className="product__middle__price--m2__p">{priceM2HTML}</p>
-        </div>
-        <div className="product__middle__price--st">
-          <p className="product__middle__price--st__p">{pricePcHTML}</p>
-        </div>
-      </div>
-
-      <div className="product__bottom">
-        <a href="${product.filepath}">
-          <p className="product__bottom__title">{product.type + ' ' + product.name}</p>
-        </a>
-        <div className="product__bottom__desc">
-          <p className="product__bottom__desc__p">Manufacturer: {product.specs?.manufacturer}</p>
-          <p className="product__bottom__desc__p">Country: {product.specs?.countryOfOrigin}</p>
-          <p className="product__bottom__desc__p">Size (mm): {product.specs?.format}</p>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-const TypeThree = ({ product, index, priceCentsPc, piecesInLinearMeter }) => {
-
-  const priceLM = (Math.ceil((priceCentsPc * piecesInLinearMeter).toFixed(4)) / 100).toFixed(2).toString();
-  const pricePc = (priceCentsPc / 100).toFixed(2).toString();
-  const indexOfDotLM = priceLM.toString().indexOf('.');
-  const indexofDotPc = pricePc.toString().indexOf('.');
-
-  const [contBlurred, setContBlurred] = useState("cont_blurred")
-  const [imgUnloaded, setImgUnloaded] = useState("img_unloaded")
-
-  const handleLoad = () => {
-    setContBlurred("")
-    setImgUnloaded("")
-  }
-
-  const [opacity, setOpacity] = useState("")
-  const [moveLeft, setMoveLeft] = useState("")
-  const [moveRight, setMoveRight] = useState("")
-
-  const handlePointerEnter = () => {
-    setOpacity("opacity")
-    timeOut = setTimeout(() => setMoveLeft("moveLeft"), 1000)
-    timeOutBack = setTimeout(() => setMoveRight("moveRight"), 5000);
-  }
-
-  const handlePointerLeave = () => {
-    setOpacity("")
-    setMoveLeft("")
-    setMoveRight("")
-    clearTimeout(timeOut);
-    clearTimeout(timeOutBack);
-  }
-
-  let priceLMHTML = (
-    <>
-      <sup>€</sup>{priceLM.slice(0, indexOfDotLM)}<span className="price-small">{priceLM.slice(indexOfDotLM)}</span> <span className="price-desc">lm</span>
-    </>
-  )
-  let pricePcHTML = (
-    <>
-      <sup>€</sup>{pricePc.slice(0, indexofDotPc)}<span className="price-small">{pricePc.slice(indexofDotPc)}</span> <span className="price-desc">pc</span>
-    </>
-  )
-
-  return (
-    <div className="product" onPointerEnter={handlePointerEnter} onPointerLeave={handlePointerLeave}>
-      <div className="product__top">
-
-        <a href={`${product.filepath}`}>
-          <div className={`product__top__cont ${contBlurred}`}>
-            <Image onLoad={handleLoad} className={`product__top__cont__img product_img_${index} ${imgUnloaded} ${opacity} ${moveLeft} ${moveRight}`} src={`${product.image_thumbnail[0]}`} alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`} width="350" height="229" loading="lazy" />
-            <Image className={`product__top__cont__img product_img_second_${index} ${opacity} ${moveLeft} ${moveRight}`} src={`${product.image_thumbnail[1]}`} alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`} width="350" height="229" loading="eager"/>
-          </div>
-        </a>
-
-        <div className="product__top__cont__stock">
-          <Image src="/images/icons/check.svg" alt="checkmark" className="stock" width="16" height="16" />
-          <p className="product__top__cont__stock__desc">{product.availability}</p>
-        </div>
-      </div>
-
-      <div className="product__middle">
-        <div className="product__middle__price--m2">
-          <p className="product__middle__price--m2__p">{priceLMHTML}</p>
-        </div>
-        <div className="product__middle__price--st">
-          <p className="product__middle__price--st__p">{pricePcHTML}</p>
-        </div>
-      </div>
-
-      <div className="product__bottom">
-        <a href={`${product.filepath}`}>
-          <p className="product__bottom__title">{product.type + ' ' + product.name}</p>
-        </a>
-        <div className="product__bottom__desc">
-          <p className="product__bottom__desc__p">Manufacturer: {product.specs?.manufacturer}</p>
-          <p className="product__bottom__desc__p">Country: {product.specs?.countryOfOrigin}</p>
-          <p className="product__bottom__desc__p">Size (mm): {product.specs?.format}</p>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-const TypeFour = ({ product, index, priceCentsPc }) => {
-  const pricePc = (priceCentsPc / 100).toFixed(2).toString();
-  const indexofDotPc = pricePc.toString().indexOf('.');
-
-  const [contBlurred, setContBlurred] = useState("cont_blurred")
-  const [imgUnloaded, setImgUnloaded] = useState("img_unloaded")
-
-  const handleLoad = () => {
-    setContBlurred("")
-    setImgUnloaded("")
-  }
-
-  const [opacity, setOpacity] = useState("")
-  const [moveLeft, setMoveLeft] = useState("")
-  const [moveRight, setMoveRight] = useState("")
-
-  const handlePointerEnter = () => {
-    setOpacity("opacity")
-    timeOut = setTimeout(() => setMoveLeft("moveLeft"), 1000)
-    timeOutBack = setTimeout(() => setMoveRight("moveRight"), 5000);
-  }
-
-  const handlePointerLeave = () => {
-    setOpacity("")
-    setMoveLeft("")
-    setMoveRight("")
-    clearTimeout(timeOut);
-    clearTimeout(timeOutBack);
-  }
-
-  let pricePcHTML = (
-    <>
-      <sup>€</sup>{pricePc.slice(0, indexofDotPc)}<span className="price-small">{pricePc.slice(indexofDotPc)}</span> <span className="price-desc">pc</span>
-    </>
-  )
-
-  return (
-    <div className="product" onPointerEnter={handlePointerEnter} onPointerLeave={handlePointerLeave}>
-      <div className="product__top">
-
-        <a href={`${product.filepath}`}>
-          <div className={`product__top__cont ${contBlurred}`}>
-            <Image onLoad={handleLoad} className={`product__top__cont__img product_img_${index} ${imgUnloaded} ${opacity} ${moveLeft} ${moveRight}`} src={`${product.image_thumbnail[0]}`} alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`} width="350" height="229" loading="lazy" />
-            <Image className={`product__top__cont__img product_img_second_${index} ${opacity} ${moveLeft} ${moveRight}`} src={`${product.image_thumbnail[1]}`} alt={`${product.type + ' ' + product.specs.manufacturer + ' ' + product.name + ' ' + product.specs.format}`} width="350" height="229" loading="eager"/>
-          </div>
-        </a>
-
-        <div className="product__top__cont__stock">
-          <Image src="/images/icons/check.svg" alt="checkmark" className="stock" width="16" height="16" />
-          <p className="product__top__cont__stock__desc">{product.availability}</p>
-        </div>
-      </div>
-
-      <div className="product__middle">
-        <div className="product__middle__price--m2">
-          <p className="product__middle__price--m2__p">{pricePcHTML}</p>
-        </div>
-        <div className="product__middle__price--st">
-          <p className="product__middle__price--st__p"></p>
-        </div>
-      </div>
-
-      <div className="product__bottom">
-        <a href="${product.filepath}">
-          <p className="product__bottom__title">{product.type + ' ' + product.name}</p>
-        </a>
-        <div className="product__bottom__desc">
-          <p className="product__bottom__desc__p">Manufacturer: {product.specs?.manufacturer}</p>
-          <p className="product__bottom__desc__p">Country: {product.specs?.countryOfOrigin}</p>
-          <p className="product__bottom__desc__p">Size (mm): {product.specs?.format}</p>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default ShowCategoryProducts
+export default ShowProducts
