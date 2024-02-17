6ab9dd731eaab6eadf64e081a99019a83bace934
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2024-02-18 00:58:05 +0400

    updated favicon and logo, added Google and Yandex counters

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import "@/scss/styles.scss";
 import Header from "@/components/general/header";
 import Footer from "@/components/general/footer";
 import { getDictionary } from "@/get-dictionary";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 export const metadata = {
@@ -16,12 +17,13 @@ export async function generateStaticParams() {
 }
 
 export default async function RootLayout({ children, params }: { children: React.ReactNode, params: { lang: Locale } }) {
-  
+
   const dictionary = await getDictionary(params.lang);
 
   return (
     <html lang={params.lang}>
       <body className="body">
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_MEASUREMENT_ID!} />
         <Header dictionary={dictionary} />
         {children}
         <Footer dictionary={dictionary} />
